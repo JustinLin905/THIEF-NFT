2d52d59aa2cda933858600af080d1afb3aef0e5d
Justin Lin <tojustinlin@gmail.com>
2023-05-19 21:25:10 -0400

    Main Menu created and animation, imported Solana SDK

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    public TextMeshProUGUI scrollingText;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,13 @@ public class MainMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Move scrolling text slowly to left
+        scrollingText.transform.Translate(Vector2.left * Time.deltaTime * 50);
+
+        if (scrollingText.transform.position.x < -1000)
+        {
+            scrollingText.transform.position = new Vector2(1800, scrollingText.transform.position.y);
+        }
     }
 }
+
