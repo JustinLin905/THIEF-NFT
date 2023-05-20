009e628de045bd95d475aa7db208ccacc431115c
Justin Lin <tojustinlin@gmail.com>
2023-05-20 18:17:43 -0400

    Working on Hacking minigame

---
+++ Assets/Scripts/HackingMinigame.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+
+public class HackingMinigame : MonoBehaviour
+{
+    public TextMeshProUGUI hackingText;
+    public TextMeshProUGUI bubbleText1;
+    public TextMeshProUGUI bubbleText2;
+    public TextMeshProUGUI bubbleText3;
+    public TextMeshProUGUI bubbleText4;
+    public TextMeshProUGUI bubbleText5;
+    public TextMeshProUGUI bubbleText6;
+    public TextMeshProUGUI bubbleText7;
+    public TextMeshProUGUI bubbleText8;
+    public TextMeshProUGUI bubbleText9;
+    public TextMeshProUGUI bubbleText10;
+    public TextMeshProUGUI bubbleText11;
+    public TextMeshProUGUI bubbleText12;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set all bubble texts to empty strings
+        bubbleText1.text = "";
+        bubbleText2.text = "";
+        bubbleText3.text = "";
+        bubbleText4.text = "";
+        bubbleText5.text = "";
+        bubbleText6.text = "";
+        bubbleText7.text = "";
+        bubbleText8.text = "";
+        bubbleText9.text = "";
+        bubbleText10.text = "";
+        bubbleText11.text = "";
+        bubbleText12.text = "";
+    }
+    
+}
